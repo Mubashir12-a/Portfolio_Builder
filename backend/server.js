bc261791537b30f6b54f5da566e868b6745e4915
mubashir12-a <ma2625645@gmail.com>
2026-04-30 19:28:59 +0530

    Update V-3.3.2 Render Setup

--- backend/server.js
+++ backend/server.js
@@ -14,9 +14,9 @@ const generateOTP = () => Math.floor(100000 + Math.random() * 900000);
 // mail setup
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
-  family: 4, 
+  port: 465,
+  secure: true, 
+  family: 4,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
