0e84dc2e0792434385b448ee27b961861aeccf5f
mubashir12-a <ma2625645@gmail.com>
2026-04-30 19:10:16 +0530

    Update V-3.3.2 Render Setup

--- backend/server.js
+++ backend/server.js
@@ -49,4 +49,8 @@ app.post("/verify-otp", (req, res) => {
   res.json({ success: false });
 });
 
-app.listen(5000, () => console.log("Server running on 5000"));
+// app.listen(5000, () => console.log("Server running on 5000"));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log("Server running on", PORT));
